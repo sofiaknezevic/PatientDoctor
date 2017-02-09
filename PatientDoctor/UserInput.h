622a832457f359c5d02afa60f2216e2d32b9c2c6
sofiaknezevic <sofia.knezevic@gmail.com>
2017-02-09 08:53:38 -0800

    Add UserInput class

---
+++ PatientDoctor/UserInput.h
@@ -0,0 +1,15 @@
+//
+//  UserInput.h
+//  PatientDoctor
+//
+//  Created by Sofia Knezevic on 2017-02-09.
+//  Copyright © 2017 Sofia Knezevic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserInput : NSObject
+
++ (NSString *) input;
+
+@end
---
+++ PatientDoctor/UserInput.m
@@ -0,0 +1,25 @@
+//
+//  UserInput.m
+//  PatientDoctor
+//
+//  Created by Sofia Knezevic on 2017-02-09.
+//  Copyright © 2017 Sofia Knezevic. All rights reserved.
+//
+
+#import "UserInput.h"
+
+@implementation UserInput
+
++ (NSString *)input
+{
+    
+    char charArray[255];
+    fgets(charArray, 255, stdin);
+    
+    return [[NSString stringWithCString:charArray
+                               encoding:NSUTF8StringEncoding]
+        stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    
+}
+
+@end
